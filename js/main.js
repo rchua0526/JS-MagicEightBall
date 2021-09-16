e373cc2e70da4cfca1ee4aef03955a37a527f67d
rchua0526 <rodeljchua@gmail.com>
2021-09-15 18:36:41 -0700

    fixed click

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
-let answers = ["It is certain", 
-"It is decidedly so", 
-"Without a doubt", 
-"Yes - definitely",
-"You may rely on it", 
-"As I see it, yes", 
-"Most likely", 
-"Outlook good", 
-"Yes", "Signs point to yes",
-"Don't count on it", 
-"My reply is no",
-"My sources say no", 
-"Outlook not so good",
-"Very doubtful", 
-"Reply hazy, try again", 
-"Ask again later", 
-"Better not tell you now",
-"Cannot predict now", 
-"Concentrate and ask again"];
-    
+let answers = ["Yes", 
+"No", 
+"Maybe so", 
+"Definitely",
+"The Force", 
+"Expecto Patronus", 
+"Who can know?", 
+"Sure", 
+"I guess?",
+"Nah", 
+"Meh",
+"HAHA", 
+"You're Batman",
+"What?", 
+"Who are you?", 
+"How about later?", 
+"Don't want to know",
+"Cannot predict now", ];
+
 window.onload = function() {
 let eight = document.getElementById("eight");
 let answer = document.getElementById("answer");
-let  eightball = document.getElementById("ball");
+let ball = document.getElementById("ball");
 let question = document.getElementById("question");
-    
-button.addEventListener("click", function() {
-    if (question.value.length < 1) {
-    alert('Enter a question!');
-    } else {
-    eight.innerText = "";
-    var num = Math.floor(Math.random() * Math.floor(answers.length));
-    answer.innerText = answers[num];
-    }
-    });
-    };
+
+ball.addEventListener("click", function() {
+if (question.value.length < 1) {
+alert('Enter a question!');
+} else {
+eight.innerText = "";
+let num = Math.floor(Math.random() * Math.floor(answers.length));
+answer.innerText = answers[num];
+}
+});
+};
