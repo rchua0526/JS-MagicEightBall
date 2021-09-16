3b5c71d9c26c50524336950e891abefec2a4028a
rchua0526 <rodeljchua@gmail.com>
2021-09-15 17:34:57 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let answers = ["It is certain", 
+"It is decidedly so", 
+"Without a doubt", 
+"Yes - definitely",
+"You may rely on it", 
+"As I see it, yes", 
+"Most likely", 
+"Outlook good", 
+"Yes", "Signs point to yes",
+"Don't count on it", 
+"My reply is no",
+"My sources say no", 
+"Outlook not so good",
+"Very doubtful", 
+"Reply hazy, try again", 
+"Ask again later", 
+"Better not tell you now",
+"Cannot predict now", 
+"Concentrate and ask again"];
+    
+window.onload = function() {
+let eight = document.getElementById("eight");
+let answer = document.getElementById("answer");
+let  eightball = document.getElementById("ball");
+let question = document.getElementById("question");
+    
+button.addEventListener("click", function() {
+    if (question.value.length < 1) {
+    alert('Enter a question!');
+    } else {
+    eight.innerText = "";
+    var num = Math.floor(Math.random() * Math.floor(answers.length));
+    answer.innerText = answers[num];
+    }
+    });
+    };
